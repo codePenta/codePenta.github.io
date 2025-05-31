ad618cbd21a3a9e39bd57881ac4db3dd66e08a65
PENTA <fhelm2001@gmail.com>
2025-05-31 23:08:15 +0200

    Added vite as a bundler, so npm modules can be used. Also added the github api (octokit) for use in the future

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello, World!");
+import { Octokit } from "octokit";
+
+const octokit: Octokit = new Octokit({
+    auth: import.meta.env.GITHUB_TOKEN
+});
+
+async function main()
+{
+    try
+    {
+        const response = await octokit.request("GET /users/{username}/repos", {
+            username: "codePenta",
+            type: "all",
+            per_page: 100,
+            sort: "updated",
+            direction: "desc"
+        });
+        const repos = response.data;
+        console.log(`Found ${repos.length} repositories for user codePenta:`);
+        repos.forEach((repo: { name: any; html_url: any; }) =>
+        {
+            console.log(`- ${repo.name}: ${repo.html_url}`);
+        });
+    } catch (error: any)
+    {
+        console.error("Error fetching repositories:", error);
+    }
+}
+
+main();
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    build: {
+        outDir: 'dist',
+        sourcemap: true,
+        rollupOptions: {
+            input: {
+                main: '/index.html'
+            }
+        }
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    server: {
+        port: 5173,
+        open: true,
+        watch: {
+            usePolling: true
+        }
+    },
+    base: '/'
+})
