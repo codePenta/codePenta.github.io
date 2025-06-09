45b8a5d777f92db3fa6e1f36576ce8653785c404
PENTA <fhelm2001@gmail.com>
2025-06-09 03:51:22 +0200

    Git api now uses the singleton pattern and api. Also the api and business logic is now separted

---
+++ src/api/github.ts
@@ -0,0 +1,24 @@
+import { GitHubService } from "./services";
+
+export async function fetchRepos()
+{
+    const octokit = GitHubService.Instance;
+
+    const params: RepoRequestParams = {
+        username: import.meta.env.VITE_GITHUB_USERNAME!,
+        type: "all",
+        per_page: 100,
+        sort: "updated",
+        direction: "desc"
+    };
+
+    try
+    {
+        const response = await octokit.request("GET /users/{username}/repos", params);
+        return response.data;
+    } catch (error)
+    {
+        console.error("Error fetching repositories:", error);
+        throw error;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,12 @@
-import { Octokit } from "octokit";
-
-const octokit: Octokit = new Octokit({
-    auth: import.meta.env.GITHUB_TOKEN
-});
-
 async function main()
 {
     try
     {
-        const response = await octokit.request("GET /users/{username}/repos", {
-            username: "codePenta",
-            type: "all",
-            per_page: 100,
-            sort: "updated",
-            direction: "desc"
-        });
-        const repos = response.data;
-        console.log(`Found ${repos.length} repositories for user codePenta:`);
-        repos.forEach((repo: { name: any; html_url: any; }) =>
-        {
-            console.log(`- ${repo.name}: ${repo.html_url}`);
-        });
-    } catch (error: any)
+        const { displayGitHubRepositories } = await import("./services/api/github");
+        await displayGitHubRepositories();
+    } catch (error)
     {
-        console.error("Error fetching repositories:", error);
+        console.error("Error in main function:", error);
     }
 }
 
